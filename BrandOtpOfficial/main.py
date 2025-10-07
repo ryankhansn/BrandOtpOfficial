854dfe7b0f2cb221e228eb83b014850c6bc0fcca
ryan <93570762+ryankhansn@users.noreply.github.com>
2025-10-07 20:32:34 +0530

    Update main.py

--- BrandOtpOfficial/main.py
+++ BrandOtpOfficial/main.py
@@ -61,7 +61,7 @@ app.add_middleware(
     allow_origins=[
         "https://brandotpofficial.shop",
         "https://www.brandotpofficial.shop",
-        "https://brandotpofficial.netlify.app/",
+        "https://brandotpofficials.netlify.app/",
         "http://localhost:8000",
         "http://127.0.0.1:5500",
     ],
@@ -656,6 +656,7 @@ if __name__ == "__main__":
         reload=False,
         log_level="info"
     )
+
 
 
 
